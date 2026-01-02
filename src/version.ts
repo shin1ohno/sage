635dbd2af0e5e8789843a46c83ebc22070596121
Shin'ichi Ohno <shin1ohno@gmail.com>
2026-01-03 01:35:47 +0900

    0.7.4

--- src/version.ts
+++ src/version.ts
@@ -7,5 +7,5 @@
 
 // Version is updated by npm version command and build process
 // Keep in sync with package.json
-export const VERSION = '0.7.3';
+export const VERSION = '0.7.4';
 export const SERVER_NAME = 'sage';
